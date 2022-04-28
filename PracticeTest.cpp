c378e5fee0f69af1c556808dee5a72c4abab0d15
Zalymo <cwatts11@mail.csuchico.edu>
2022-04-27 20:06:59 -0700

    added assertion tests for the Practice.cpp functions to ensure full coverage: did EQ and NE tests for every function

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -8,13 +8,31 @@
 class PracticeTest : public ::testing::Test
 {
 	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+		PracticeTest() {} //constructor runs before each test
+		virtual ~PracticeTest() {
+
+		} //destructor cleans up after tests
+		virtual void SetUp() {
+			
+		} //sets up before each test (after constructor)
+		virtual void TearDown() {
+
+		} //clean up after each test, (before destructor)
+
+		//Practice test_object;
 };
 
 TEST(PracticeTest, smoke_test)
 {
-    ASSERT_TRUE(true);
+    Practice test_object;
+	ASSERT_TRUE(true);
+
+	int a[7] = {0, 2, 3, 4, 5, 6, 7};
+	ASSERT_EQ(test_object.allnighter(a), &a[0]);
+	ASSERT_NE(test_object.allnighter(a), nullptr);
+	
+	ASSERT_EQ(test_object.count_starting_repeats("aaaa"), 3);
+	ASSERT_NE(test_object.count_starting_repeats("aaaa"), 0);
+
+	ASSERT_EQ(test_object.isPalindrome("racecar"), true);
 }
